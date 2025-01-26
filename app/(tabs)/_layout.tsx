14f6f47f255d78680e18a65f69f6e3a6e6e32683
hassan <hassan@hassans-MacBook-Pro.local>
2025-01-26 20:45:07 +0100

    ad

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -131,12 +131,3 @@ const TabsLayout = () => {
 
 export default TabsLayout;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    height: 100,
-    marginTop: -20,
-  },
-});
